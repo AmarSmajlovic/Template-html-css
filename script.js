dbd57515ce9175182da74d9be59af5e40da2dab8
AmarSmajlovic <amarcsgo@gmail.com>
2021-09-30 19:48:43 +0200

    add mask-group-container and fix header on scroll

---
+++ script.js
@@ -0,0 +1,6 @@
+//logic for scroll fixed header
+window.onscroll = () => {
+   let header = document.getElementById('header');
+   if(window.pageYOffset> header.offsetTop) header.classList.add('scroll')
+   else header.classList.remove('scroll');
+}
